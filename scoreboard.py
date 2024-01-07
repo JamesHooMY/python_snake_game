893154223c7813cb2b17c0bdb4a5cd13bcaab67f
JamesHooMY <hoosinyong@hotmail.com>
2024-01-07 19:57:40 +0800

    feat: add snake game

---
+++ scoreboard.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+
+ALIGMENT = "center"
+FONT = ("Arial", 12, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.penup()
+        self.color("white")
+        self.goto(0, 270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGMENT, font=FONT)
+
+    def reset(self):
+        self.score = 0
+        self.update_scoreboard()
